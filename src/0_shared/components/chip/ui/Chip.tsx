1dfd4eb857d1415f770b50715528ac1ab542e918
Дмитрий Бажаев <48925297+qb-wht@users.noreply.github.com>
2024-12-31 15:30:57 +0300

    Feat: Add chip component

---
+++ src/0_shared/components/chip/ui/Chip.tsx
@@ -0,0 +1,20 @@
+import { DesignSystem, PropsOf, Variant } from '@/0_shared/types';
+import s from './Chip.module.css';
+import { cn } from '@/0_shared/utils';
+
+export type ChipProps = { variant: Variant } & PropsOf<HTMLDivElement> & DesignSystem;
+
+export const Chip = (props: ChipProps) => {
+  const { color = 'primary', size = 'l', borderRadius = 'medium', gap = '1', children, variant = 'fill' } = props;
+
+  const classNames = cn(
+    s.chip,
+    s[`variant-${variant}`],
+    s[`color-${color}`],
+    `size-${size}`,
+    `gap-${gap}`,
+    `br-${borderRadius}`
+  );
+
+  return <div className={classNames.build()} {...props} />;
+};
---
+++ src/app/chips/page.tsx
@@ -0,0 +1,144 @@
+import s from '@/0_shared/styles/shared.module.css';
+import l from '@/0_shared/styles/layout.module.css';
+import { cn } from '@/0_shared/utils';
+import { Chip } from '@/0_shared/components/chip';
+
+export const Chips = () => {
+  return (
+    <div style={{ gap: '8px' }} className={cn(s.page, l.column).build()}>
+      <h1>Border radius</h1>
+
+      <Chip color="primary" variant="fill" borderRadius="none">
+        None
+      </Chip>
+
+      <Chip color="primary" variant="fill" borderRadius="small">
+        Small
+      </Chip>
+
+      <Chip color="primary" variant="fill" borderRadius="medium">
+        Medium
+      </Chip>
+
+      <Chip color="primary" variant="fill" borderRadius="round">
+        Medium
+      </Chip>
+
+      <Chip color="primary" variant="fill" borderRadius="circle">
+        Circle
+      </Chip>
+
+      <h1>Variant</h1>
+
+      <Chip color="primary" variant="fill" borderRadius="round">
+        Fill
+      </Chip>
+
+      <Chip color="primary" variant="fillLight" borderRadius="round">
+        FillLight
+      </Chip>
+
+      <Chip color="primary" variant="outline" borderRadius="round">
+        Outline
+      </Chip>
+
+      <Chip color="primary" variant="ghost" borderRadius="round">
+        Ghost
+      </Chip>
+
+      <h1>Color</h1>
+
+      <Chip color="secondary" variant="fill" borderRadius="round">
+        Fill
+      </Chip>
+
+      <Chip color="secondary" variant="fillLight" borderRadius="round">
+        FillLight
+      </Chip>
+
+      <Chip color="secondary" variant="outline" borderRadius="round">
+        Outline
+      </Chip>
+
+      <Chip color="secondary" variant="ghost" borderRadius="round">
+        Ghost
+      </Chip>
+
+      <h1>Size</h1>
+
+      <Chip size="s" color="secondary" variant="fillLight" borderRadius="round">
+        Chip S
+      </Chip>
+
+      <Chip size="m" color="secondary" variant="outline" borderRadius="round">
+        Chip M
+      </Chip>
+
+      <Chip size="l" color="secondary" variant="ghost" borderRadius="round">
+        Chip L
+      </Chip>
+
+      <Chip size="xl" color="secondary" variant="ghost" borderRadius="round">
+        Chip XL
+      </Chip>
+
+      <Chip size="xxl" color="secondary" variant="ghost" borderRadius="round">
+        Chip XXL
+      </Chip>
+
+      <h1>Gap</h1>
+
+      <Chip gap="1" color="primary" variant="fillLight" borderRadius="round">
+        <div>Gap</div>
+        <div>1</div>
+      </Chip>
+
+      <Chip gap="2" color="primary" variant="outline" borderRadius="round">
+        <div>Gap</div>
+        <div>2</div>
+      </Chip>
+
+      <Chip gap="3" color="primary" variant="ghost" borderRadius="round">
+        <div>Gap</div>
+        <div>3</div>
+      </Chip>
+
+      <Chip gap="4" color="primary" variant="ghost" borderRadius="round">
+        <div>Gap</div>
+        <div>4</div>
+      </Chip>
+
+      <Chip gap="5" color="primary" variant="ghost" borderRadius="round">
+        <div>Gap</div>
+        <div>5</div>
+      </Chip>
+
+      <Chip gap="6" color="primary" variant="fillLight" borderRadius="round">
+        <div>Gap</div>
+        <div>6</div>
+      </Chip>
+
+      <Chip gap="7" color="primary" variant="outline" borderRadius="round">
+        <div>Gap</div>
+        <div>7</div>
+      </Chip>
+
+      <Chip gap="8" color="primary" variant="ghost" borderRadius="round">
+        <div>Gap</div>
+        <div>8</div>
+      </Chip>
+
+      <Chip gap="9" color="primary" variant="ghost" borderRadius="round">
+        <div>Gap</div>
+        <div>9</div>
+      </Chip>
+
+      <Chip gap="10" color="primary" variant="ghost" borderRadius="round">
+        <div>Gap</div>
+        <div>10</div>
+      </Chip>
+    </div>
+  );
+};
+
+export default Chips;
